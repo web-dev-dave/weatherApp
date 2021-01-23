1308a9bd7b9a1b99ae38a4c020695ec599d0170a
David Steel <65894147+web-dev-dave@users.noreply.github.com>
2021-01-24 10:45:51 +1300

    display weather

--- ui.js
+++ ui.js
@@ -6,14 +6,19 @@ class UI {
     this.details = document.getElementById('w-details')
     this.icon = document.getElementById('w-icon')
     this.humidity = document.getElementById('w-humidity')
-    this.dewpoint = document.getElementById('w-dewpoint')
+    this.windDir = document.getElementById('w-wind-dir')
     this.feelsLike = document.getElementById('w-feels-like')
-    this.wind = document.getElementById('w-wind')
+    this.windSpd = document.getElementById('w-wind-spd')
   }
 
   paint(weather) {
     this.location.textContent = weather.name
     this.description.textContent = weather.weather[0].description
     this.string.textContent = weather.main.temp + '°C'
+    this.icon.setAttribute('src', `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`)
+    this.humidity.textContent = `Humidity: ${weather.main.humidity}%`
+    this.windDir.textContent = `Wind Direction: ${weather.wind.deg}°`
+    this.feelsLike.textContent = `Feels Like: ${weather.main.feels_like}°C`
+    this.windSpd.textContent = `Wind Speed: ${weather.wind.speed}m/s`
   }
 }
